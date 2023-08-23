9076ec6e2a6c1b4f8ee44b5e50d7f98d33c0fd75
glapettina <guillermo@glsoftware.com.ar>
2023-08-23 14:56:27 -0300

    Permitir guion medio en nombre de usuario

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -11,7 +11,7 @@ class ControladorUsuarios{
 
 		if (isset($_POST["ingUsuario"])) {
 			
-			if (preg_match('/^[a-zA-Z0-9_.@]+$/', $_POST["ingUsuario"]) &&
+			if (preg_match('/^[a-zA-Z0-9_\-.@]+$/', $_POST["ingUsuario"]) &&
 				preg_match('/^[a-zA-Z0-9*]+$/', $_POST["ingPassword"])) {
 
 				$encriptar = crypt($_POST["ingPassword"], '$2a$07$usesomesillystringforsalt$');
